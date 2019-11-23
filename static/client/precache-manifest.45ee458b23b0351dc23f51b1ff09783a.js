5ba16f4cf679454593414f7c010ae9d6cd304d8e
i.n.g.o <ingo.randolf@servus.at>
2019-11-23 18:38:28 +0100

    add client

---
+++ static/client/precache-manifest.45ee458b23b0351dc23f51b1ff09783a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0bcd5b9effbe2570868f8c906ea7792",
+    "url": "/client/static/media/icons-16.d0bcd5b9.woff"
+  },
+  {
+    "revision": "c744155f57c46170c82c",
+    "url": "/client/static/css/main.d0619907.chunk.css"
+  },
+  {
+    "revision": "0ec8ac2bacb2c3a1b2a2064305bc1223",
+    "url": "/client/static/media/icons-20.0ec8ac2b.ttf"
+  },
+  {
+    "revision": "4c8cadfd573a19e52fc1",
+    "url": "/client/static/js/1.4c8cadfd.chunk.js"
+  },
+  {
+    "revision": "dad60270bf4017963b16",
+    "url": "/client/static/js/runtime~main.dad60270.js"
+  },
+  {
+    "revision": "26e98f292f486f2bfac2abf9472e490a",
+    "url": "/client/static/media/icons-16.26e98f29.ttf"
+  },
+  {
+    "revision": "c744155f57c46170c82c",
+    "url": "/client/static/js/main.c744155f.chunk.js"
+  },
+  {
+    "revision": "5dbae370414339a081c1c6fac80659cc",
+    "url": "/client/static/media/icons-16.5dbae370.eot"
+  },
+  {
+    "revision": "9d0578e5315c18aa528db1f1bfcb56f5",
+    "url": "/client/static/media/icons-20.9d0578e5.woff"
+  },
+  {
+    "revision": "f4d910a0626d8e60f310a7199dc6b191",
+    "url": "/client/static/media/icons-20.f4d910a0.eot"
+  },
+  {
+    "revision": "4c8cadfd573a19e52fc1",
+    "url": "/client/static/css/1.69d2d30a.chunk.css"
+  },
+  {
+    "revision": "8a2237dccdde0426cae5795704588d89",
+    "url": "/client/index.html"
+  }
+];
