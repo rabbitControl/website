81b540bd755bd2fd67d0ff0e8cb6eabf8b34d21c
i.n.g.o <ingo.randolf@servus.at>
2020-04-06 11:44:21 +0200

    update client

--- static/client/precache-manifest.0a4b10f32984b5174490f19b46b564ad.js
+++ static/client/precache-manifest.0a4b10f32984b5174490f19b46b564ad.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "5d7ed02f07bef8cfba3b",
+    "revision": "2239bb277953e2d7084a",
     "url": "/client/static/css/main.46421d9b.chunk.css"
   },
   {
-    "revision": "5d7ed02f07bef8cfba3b",
-    "url": "/client/static/js/main.5d7ed02f.chunk.js"
+    "revision": "2239bb277953e2d7084a",
+    "url": "/client/static/js/main.2239bb27.chunk.js"
   },
   {
     "revision": "837089123c09ea714a03",
@@ -20,20 +20,20 @@ self.__precacheManifest = [
     "url": "/client/static/js/runtime~main.dad60270.js"
   },
   {
-    "revision": "d0bcd5b9effbe2570868f8c906ea7792",
-    "url": "/client/static/media/icons-16.d0bcd5b9.woff"
-  },
-  {
     "revision": "5dbae370414339a081c1c6fac80659cc",
     "url": "/client/static/media/icons-16.5dbae370.eot"
+  },
+  {
+    "revision": "26e98f292f486f2bfac2abf9472e490a",
+    "url": "/client/static/media/icons-16.26e98f29.ttf"
   },
   {
     "revision": "f4d910a0626d8e60f310a7199dc6b191",
     "url": "/client/static/media/icons-20.f4d910a0.eot"
   },
   {
-    "revision": "26e98f292f486f2bfac2abf9472e490a",
-    "url": "/client/static/media/icons-16.26e98f29.ttf"
+    "revision": "d0bcd5b9effbe2570868f8c906ea7792",
+    "url": "/client/static/media/icons-16.d0bcd5b9.woff"
   },
   {
     "revision": "9d0578e5315c18aa528db1f1bfcb56f5",
@@ -44,7 +44,7 @@ self.__precacheManifest = [
     "url": "/client/static/media/icons-20.0ec8ac2b.ttf"
   },
   {
-    "revision": "bb425ce4a285d1fda6baf7817913a1cc",
+    "revision": "dbcb1a91f5c4b6fd2bf659d82f4f093d",
     "url": "/client/index.html"
   }
 ];
--- static/client/service-worker.js
+++ static/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/client/precache-manifest.3093156342f1be02d47b09416d7bb05e.js"
+  "/client/precache-manifest.0a4b10f32984b5174490f19b46b564ad.js"
 );
 
 workbox.clientsClaim();
